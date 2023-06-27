b67923666bf51709c30d8af3d6725ab1e691c6cb
seemix <semikhalchuk@gmail.com>
2023-06-27 22:18:27 +0300

    freeShipping

--- front/src/config/config.js
+++ front/src/config/config.js
@@ -1,6 +1,6 @@
 export const config = {
-    BACKEND_URL: 'http://localhost:5000',
-//   BACKEND_URL: 'https://test.baptist.org.md',
+   // BACKEND_URL: 'http://localhost:5000',
+   BACKEND_URL: 'https://test.baptist.org.md',
     NO_IMAGE: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png',
     CURRENCY: 'DKK',
     SHIPPING_COST: 40,
