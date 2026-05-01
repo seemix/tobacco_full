9c97bcb8c41439cc4c850bffbd9bafa93f0b911e
seemix <91602641+seemix@users.noreply.github.com>
2026-05-01 05:36:45 +0300

    removed env file

--- front/src/validators/brand-form.validator.js
+++ front/src/validators/brand-form.validator.js
@@ -3,5 +3,4 @@ import Joi from 'joi';
 export const brandFormValidator = Joi.object({
     name: Joi.string().min(3).message('minimal length is 3 symbols')
         .max(25).message('maximal length is 25 symbols')
-        .alphanum()
 });
